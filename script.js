30a27255c41e915bcd5001f97d3dd3501990c0a9
Douglas <douglas.alexandre716@gmail.com>
2021-08-28 23:14:51 -0300

    adicionado modal para exibir informações dos Pokémons

--- script.js
+++ script.js
@@ -1,19 +1,37 @@
 const apiurl = "https://pokeapi.co/api/v2"
+
+const fecharModal = () => {
+    const modal = document.querySelector(".modal-conteiner");
+    modal.remove()
+}
+
+const exibirDadosPokemon = (pokemon) => {
+    const modalConteiner = document.createElement("div")
+    modalConteiner.onclick = fecharModal
+    modalConteiner.classList.add("modal-conteiner")
+    modalConteiner.innerHTML = `<div onclick="event.stopPropagation()" class="modal">
+        <h3>${pokemon.name}</h3>
+        <img src="${pokemon["sprites"]["front_default"]}">
+    </div>`
+    document.body.appendChild(modalConteiner);
+}
+
 const capturarPokemons = async () => {
     const resposta = await fetch(`${apiurl}/pokemon`);
     const dadosPokemons = await resposta.json();
     const pokemons = dadosPokemons.results;
     const conteinerPokemons = document.querySelector(".conteiner-pokemons");
-    console.log(conteinerPokemons);
+
     for (let pokemon of pokemons) {
         const conteinerPokemon = document.createElement("div");
         conteinerPokemon.classList.add("conteiner-pokemon");
         const completedPokemonResponse = await fetch(pokemon.url)
         const completedPokemon = await completedPokemonResponse.json()
-        console.log(completedPokemon)
-        conteinerPokemon.innerHTML = `<img src="${completedPokemon["sprites"]["front_default"]}"/>`;
+
+        conteinerPokemon.innerHTML = `<img onclick='exibirDadosPokemon(${JSON.stringify(completedPokemon)})' src="${completedPokemon["sprites"]["front_default"]}"/>`;
         conteinerPokemons.appendChild(conteinerPokemon);
     }
+
 }
 
 window.onload = capturarPokemons
